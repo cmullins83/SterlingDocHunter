f11fb72597544a5ba9df75699fa154cde88fea7e
madmullins <chris@madmullins.com>
2016-09-01 18:48:56 -0500

    Fixed icon

--- SterlingDocHunter/Query.Designer.vb
+++ SterlingDocHunter/Query.Designer.vb
@@ -22,47 +22,138 @@ Partial Class Query
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Query))
         Me.Button1 = New System.Windows.Forms.Button()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 139)
+        Me.Button1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Button1.Location = New System.Drawing.Point(12, 478)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(380, 26)
-        Me.Button1.TabIndex = 1
+        Me.Button1.Size = New System.Drawing.Size(289, 37)
+        Me.Button1.TabIndex = 2
         Me.Button1.Text = "Search"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'ListBox1
         '
+        Me.ListBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ListBox1.FormattingEnabled = True
         Me.ListBox1.ItemHeight = 16
-        Me.ListBox1.Items.AddRange(New Object() {"Results Here"})
-        Me.ListBox1.Location = New System.Drawing.Point(12, 45)
+        Me.ListBox1.Items.AddRange(New Object() {" "})
+        Me.ListBox1.Location = New System.Drawing.Point(13, 127)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(380, 84)
-        Me.ListBox1.TabIndex = 2
+        Me.ListBox1.Size = New System.Drawing.Size(600, 340)
+        Me.ListBox1.TabIndex = 3
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(13, 13)
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(13, 99)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(379, 22)
-        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Size = New System.Drawing.Size(599, 22)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(12, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(326, 81)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "Select Directory"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'FolderBrowserDialog1
+        '
+        Me.FolderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer
+        Me.FolderBrowserDialog1.SelectedPath = "C:\EDIDATA"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(439, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(85, 81)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(530, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(82, 81)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 6
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(344, 12)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(89, 81)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 8
+        Me.PictureBox3.TabStop = False
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Button3.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Button3.Location = New System.Drawing.Point(323, 478)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(289, 37)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Copy to Clipboard"
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'Query
         '
+        Me.AcceptButton = Me.Button2
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(404, 177)
+        Me.ClientSize = New System.Drawing.Size(624, 527)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Query"
         Me.Text = "Search for Document"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -70,4 +161,10 @@ Partial Class Query
     Friend WithEvents Button1 As Button
     Friend WithEvents ListBox1 As ListBox
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Button3 As Button
 End Class
--- SterlingDocHunter/Query.vb
+++ SterlingDocHunter/Query.vb
@@ -1,13 +1,55 @@
 ﻿Imports System.IO
+
 Public Class Query
+    Public SDirectory As String = "C:\EDIDATA\ArchiveIn"
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim list As System.Collections.ObjectModel.
+        Button1.Text = "Searching..."
+        Button1.Enabled = False
+        ListBox1.Items.Clear()
+        Dim a As System.Collections.ObjectModel.
   ReadOnlyCollection(Of String)
-        list = My.Computer.FileSystem.FindInFiles("C:\Test",
-        TextBox1.Text, True, FileIO.SearchOption.SearchTopLevelOnly)
-        For Each File In list
-            ListBox1.Items.Add(File)
+        a = My.Computer.FileSystem.FindInFiles(SDirectory,
+        TextBox1.Text, True, FileIO.SearchOption.SearchAllSubDirectories)
+        For Each b In a
+            ListBox1.Items.Add(b)
         Next
-        ListBox1.Items.Add("Search Complete")
+        Button1.Enabled = True
+        Button1.Text = "Search"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            SDirectory = FolderBrowserDialog1.SelectedPath
+            If SDirectory <> "" Then
+                AcceptButton = Button1
+            Else
+                AcceptButton = Button2
+            End If
+        End If
+    End Sub
+
+    Private Sub ListBox1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles ListBox1.MouseDoubleClick
+        Dim fullPath = Path.Combine(ListBox1.SelectedItem.ToString())
+        System.Diagnostics.Process.Start("C:\Program Files (x86)\Notepad++\notepad++.exe", fullPath)
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        System.Diagnostics.Process.Start("http://www.sterlingpublishing.com/catalogs.html")
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        System.Diagnostics.Process.Start("http://www.coenterprise.com")
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        System.Diagnostics.Process.Start("http://www.syncrofy.com/")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim CB As String
+        Clipboard.Clear()
+        CB = ListBox1.Text
+        Clipboard.SetText(CB)
     End Sub
 End Class
