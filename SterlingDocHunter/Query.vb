4d53af7102124e9ab90d4bd04f901c3d11dcca87
madmullins <chris@madmullins.com>
2016-09-02 08:32:36 -0500

    Cleaned up whitespace

--- SterlingDocHunter/Query.vb
+++ SterlingDocHunter/Query.vb
@@ -1,5 +1,4 @@
 ﻿Imports System.IO
-
 Public Class Query
     Public SDirectory As String = "C:\EDIDATA\ArchiveIn"
     Dim tbt As String = "clear"
@@ -17,9 +16,7 @@ Public Class Query
         Button1.Enabled = True
         Button1.Text = "Search"
     End Sub
-
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-
         If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
             SDirectory = FolderBrowserDialog1.SelectedPath
             If SDirectory <> "" Then
@@ -29,41 +26,29 @@ Public Class Query
             End If
         End If
     End Sub
-
     Private Sub ListBox1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles ListBox1.MouseDoubleClick
         Dim fullPath = Path.Combine(ListBox1.SelectedItem.ToString())
         System.Diagnostics.Process.Start("C:\Program Files (x86)\Notepad++\notepad++.exe", fullPath)
     End Sub
-
     Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
         System.Diagnostics.Process.Start("http://www.sterlingpublishing.com/catalogs.html")
     End Sub
-
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
         System.Diagnostics.Process.Start("http://www.coenterprise.com")
     End Sub
-
     Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
         System.Diagnostics.Process.Start("http://www.syncrofy.com/")
     End Sub
-
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Dim CB As String
         Clipboard.Clear()
         CB = ListBox1.Text
         Clipboard.SetText(CB)
     End Sub
-
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
-    End Sub
-
     Private Sub TextBox1_MouseClick(sender As Object, e As MouseEventArgs) Handles TextBox1.MouseClick
         If tbt = "clear" Then
             TextBox1.Text = ""
             tbt = ""
         End If
     End Sub
-
-
 End Class
