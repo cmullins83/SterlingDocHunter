dbfc1d4fa435362eb4f84f8b23f4b7b376bade72
Christopher <chris@madmullins.com>
2016-09-19 17:58:02 -0500

    Major overhauls, including processing CSV files.

--- SterlingDocHunter/Query.Designer.vb
+++ SterlingDocHunter/Query.Designer.vb
@@ -33,9 +33,18 @@ Partial Class Query
         Me.PictureBox3 = New System.Windows.Forms.PictureBox()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SearchToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StringToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FileToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CSVToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button1
@@ -43,10 +52,10 @@ Partial Class Query
         Me.Button1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.Button1.Location = New System.Drawing.Point(8, 387)
+        Me.Button1.Location = New System.Drawing.Point(8, 502)
         Me.Button1.Margin = New System.Windows.Forms.Padding(2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(158, 30)
+        Me.Button1.Size = New System.Drawing.Size(375, 30)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "Search"
         Me.Button1.UseVisualStyleBackColor = True
@@ -58,10 +67,10 @@ Partial Class Query
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ListBox1.FormattingEnabled = True
         Me.ListBox1.Items.AddRange(New Object() {" "})
-        Me.ListBox1.Location = New System.Drawing.Point(10, 103)
+        Me.ListBox1.Location = New System.Drawing.Point(10, 129)
         Me.ListBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(451, 277)
+        Me.ListBox1.Size = New System.Drawing.Size(668, 355)
         Me.ListBox1.TabIndex = 3
         '
         'TextBox1
@@ -69,21 +78,21 @@ Partial Class Query
         Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.TextBox1.Location = New System.Drawing.Point(10, 80)
+        Me.TextBox1.Location = New System.Drawing.Point(10, 96)
         Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(450, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(667, 20)
         Me.TextBox1.TabIndex = 1
-        Me.TextBox1.Text = "Enter Search String"
+        Me.TextBox1.Text = "Enter Search String or Select Input File"
         '
         'Button2
         '
         Me.Button2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Button2.Location = New System.Drawing.Point(9, 10)
+        Me.Button2.Location = New System.Drawing.Point(7, 26)
         Me.Button2.Margin = New System.Windows.Forms.Padding(2)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(244, 66)
+        Me.Button2.Size = New System.Drawing.Size(461, 66)
         Me.Button2.TabIndex = 0
         Me.Button2.Text = "Select Directory"
         Me.Button2.UseVisualStyleBackColor = True
@@ -98,7 +107,7 @@ Partial Class Query
         Me.PictureBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(329, 10)
+        Me.PictureBox1.Location = New System.Drawing.Point(543, 26)
         Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(64, 66)
@@ -111,7 +120,7 @@ Partial Class Query
         Me.PictureBox2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
-        Me.PictureBox2.Location = New System.Drawing.Point(398, 10)
+        Me.PictureBox2.Location = New System.Drawing.Point(612, 26)
         Me.PictureBox2.Margin = New System.Windows.Forms.Padding(2)
         Me.PictureBox2.Name = "PictureBox2"
         Me.PictureBox2.Size = New System.Drawing.Size(62, 66)
@@ -124,7 +133,7 @@ Partial Class Query
         Me.PictureBox3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.PictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
-        Me.PictureBox3.Location = New System.Drawing.Point(258, 10)
+        Me.PictureBox3.Location = New System.Drawing.Point(472, 26)
         Me.PictureBox3.Margin = New System.Windows.Forms.Padding(2)
         Me.PictureBox3.Name = "PictureBox3"
         Me.PictureBox3.Size = New System.Drawing.Size(67, 66)
@@ -138,10 +147,10 @@ Partial Class Query
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.Button3.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.Button3.Location = New System.Drawing.Point(167, 387)
+        Me.Button3.Location = New System.Drawing.Point(387, 502)
         Me.Button3.Margin = New System.Windows.Forms.Padding(2)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(158, 30)
+        Me.Button3.Size = New System.Drawing.Size(155, 30)
         Me.Button3.TabIndex = 4
         Me.Button3.Text = "Copy to Clipboard"
         Me.Button3.UseVisualStyleBackColor = True
@@ -152,7 +161,7 @@ Partial Class Query
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Button4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.Button4.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.Button4.Location = New System.Drawing.Point(329, 387)
+        Me.Button4.Location = New System.Drawing.Point(546, 502)
         Me.Button4.Margin = New System.Windows.Forms.Padding(2)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(132, 30)
@@ -160,12 +169,60 @@ Partial Class Query
         Me.Button4.Text = "Sidetrack"
         Me.Button4.UseVisualStyleBackColor = True
         '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(685, 24)
+        Me.MenuStrip1.TabIndex = 10
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SearchToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'SearchToolStripMenuItem
+        '
+        Me.SearchToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StringToolStripMenuItem, Me.FileToolStripMenuItem1})
+        Me.SearchToolStripMenuItem.Name = "SearchToolStripMenuItem"
+        Me.SearchToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.SearchToolStripMenuItem.Text = "Search Method"
+        '
+        'StringToolStripMenuItem
+        '
+        Me.StringToolStripMenuItem.Name = "StringToolStripMenuItem"
+        Me.StringToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.StringToolStripMenuItem.Text = "String"
+        '
+        'FileToolStripMenuItem1
+        '
+        Me.FileToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ListToolStripMenuItem, Me.CSVToolStripMenuItem})
+        Me.FileToolStripMenuItem1.Name = "FileToolStripMenuItem1"
+        Me.FileToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.FileToolStripMenuItem1.Text = "File"
+        '
+        'ListToolStripMenuItem
+        '
+        Me.ListToolStripMenuItem.Name = "ListToolStripMenuItem"
+        Me.ListToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ListToolStripMenuItem.Text = "List"
+        '
+        'CSVToolStripMenuItem
+        '
+        Me.CSVToolStripMenuItem.Name = "CSVToolStripMenuItem"
+        Me.CSVToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.CSVToolStripMenuItem.Text = "CSV"
+        '
         'Query
         '
         Me.AcceptButton = Me.Button2
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(468, 428)
+        Me.ClientSize = New System.Drawing.Size(685, 543)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.PictureBox3)
@@ -175,13 +232,17 @@ Partial Class Query
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Query"
         Me.Text = "Search for Document"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -196,4 +257,12 @@ Partial Class Query
     Friend WithEvents PictureBox3 As PictureBox
     Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SearchToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StringToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FileToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ListToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CSVToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
 End Class
--- SterlingDocHunter/Query.vb
+++ SterlingDocHunter/Query.vb
@@ -1,58 +1,116 @@
 ﻿Imports System.IO
 Public Class Query
-    'My Defines go here
+    'My Defines go here, most will be overwritten when a configuration file is implimented.
     Dim CB As String
     Public SDirectory As String = ""
     Dim tbt As String = "clear"
     Dim path2 As String = "c:\TestSidetrack\"
     Dim Editor As String = "C:\Program Files (x86)\Notepad++\notepad++.exe"
-    Dim Settings As String = "C:\Boot.ini"
+    Dim Settings As String = "C:\test.ini"
+    Dim InputType As String = "String"
+    Dim CSVFile As String = ""
+    Dim Delimiter As String = ""
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Change the button so you don't think the program has hung
         Button1.Text = "Searching..."
+        'Disable the button to prevent restarting the search by mistake
         Button1.Enabled = False
+        'Empty any previous results
         ListBox1.Items.Clear()
-        Dim a As System.Collections.ObjectModel.
-  ReadOnlyCollection(Of String)
-        a = My.Computer.FileSystem.FindInFiles(SDirectory,
-        TextBox1.Text, True, FileIO.SearchOption.SearchAllSubDirectories)
-        For Each b In a
-            ListBox1.Items.Add(b)
-        Next
-        Button1.Enabled = True
-        Button1.Text = "Search"
+        'If the input type is string then we search for the string, looping through all files
+        If InputType = "String" Then
+            Dim a As System.Collections.ObjectModel.ReadOnlyCollection(Of String)
+            a = My.Computer.FileSystem.FindInFiles(SDirectory,
+            TextBox1.Text, True, FileIO.SearchOption.SearchAllSubDirectories)
+            For Each b In a
+                ListBox1.Items.Add(b)
+            Next
+            Button1.Enabled = True
+            Button1.Text = "Search"
+        End If
+        'Placeholder for CSV
+        If InputType = "CSV" Then
+            Using MyReader As New Microsoft.VisualBasic.
+                      FileIO.TextFieldParser(CSVFile)
+                MyReader.TextFieldType = FileIO.FieldType.Delimited
+                MyReader.SetDelimiters(delimiter)
+                Dim currentRow As String()
+                While Not MyReader.EndOfData
+                    Try
+                        currentRow = MyReader.ReadFields()
+                        Dim currentField As String
+                        For Each currentField In currentRow
+                            'try to recycle the code from the textbox search
+                            Dim a As System.Collections.ObjectModel.ReadOnlyCollection(Of String)
+                            a = My.Computer.FileSystem.FindInFiles(SDirectory,
+                            currentField, True, FileIO.SearchOption.SearchAllSubDirectories)
+                            Dim counter As Int16 = 0
+                            For Each b In a
+                                If counter = 0 Then
+                                    ListBox1.Items.Add(currentField + " in")
+                                End If
+                                ListBox1.Items.Add(b)
+                                counter = counter + 1
+
+                            Next
+                            ListBox1.Items.Add(vbNewLine)
+                            'end of recycling
+                        Next
+                    Catch ex As Microsoft.VisualBasic.
+                                FileIO.MalformedLineException
+                        MsgBox("Line " & ex.Message &
+                        "is not valid and will be skipped.")
+                    End Try
+                End While
+            End Using
+        End If
+        'Newline
+
+
     End Sub
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Select directory and store it to variable SDirectory
         If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
             SDirectory = FolderBrowserDialog1.SelectedPath
+            'If SDirectory isn't empty then change the default enter button to the search button (pretty nifty trick if I say so)
             If SDirectory <> "" Then
                 AcceptButton = Button1
+                'Show the working directory on the directory select button
                 Button2.Text = SDirectory + Environment.NewLine + "Click to Change"
             Else
+                'If no directory has been set then the default enter button is select directory
                 AcceptButton = Button2
                 Button2.Text = "Select Directory"
             End If
         End If
     End Sub
     Private Sub ListBox1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles ListBox1.MouseDoubleClick
+        'set the variable fullpath to be the fully qualified path of the double clicked file
         Dim fullPath = Path.Combine(ListBox1.SelectedItem.ToString())
+        'launch the fully qualified path to the editor and pass it the fully qualified path we set for the file
         System.Diagnostics.Process.Start(Editor, fullPath)
     End Sub
     Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        'open the default browser to the URL:
         System.Diagnostics.Process.Start("http://www.sterlingpublishing.com/catalogs.html")
     End Sub
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        'open the default browser to the URL:
         System.Diagnostics.Process.Start("http://www.coenterprise.com")
     End Sub
     Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        'open the default browser to the URL:
         System.Diagnostics.Process.Start("http://www.syncrofy.com/")
     End Sub
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-
+        'clear the clipboard
         Clipboard.Clear()
-        CB = ListBox1.Text
-        Clipboard.SetText(CB)
+        'set the clipboard to the contents of Listbox1
+        Clipboard.SetText(ListBox1.Text)
     End Sub
     Private Sub TextBox1_MouseClick(sender As Object, e As MouseEventArgs) Handles TextBox1.MouseClick
+        'on clicking the textbox, clear the contents
         If tbt = "clear" Then
             TextBox1.Text = ""
             tbt = ""
@@ -60,6 +118,9 @@ Public Class Query
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'this is the current iteration of the sidetrack process, Need to add support for multiple files and move out every file first
+        'then move the selected files back
+        'then we need a button to restore the files back to the original directory afterwards.
         CB = ListBox1.Text
         Dim filename As String
         filename = Path.GetFileName(CB)
@@ -67,8 +128,10 @@ Public Class Query
     End Sub
 
     Private Sub Query_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim objStreamReader As StreamReader
-        Dim strLine As String
+        'this entire section is commented out, it's development for using a configuration file
+
+        'Dim objStreamReader As StreamReader
+        'Dim strLine As String
 
         'Pass the file path and the file name to the StreamReader constructor.
         'objStreamReader = New StreamReader(Settings)
@@ -91,5 +154,25 @@ Public Class Query
 
         'Console.ReadLine()
 
+    End Sub
+
+    Private Sub CSVToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CSVToolStripMenuItem.Click
+        InputType = "CSV"
+        Delimiter = ","
+        OpenFileDialog1.Filter = "CSV Files |*.csv;*.CSV"
+        OpenFileDialog1.ShowDialog()
+        CSVFile = OpenFileDialog1.FileName
+    End Sub
+
+    Private Sub SearchToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SearchToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub ListToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ListToolStripMenuItem.Click
+        InputType = "newline"
+        Delimiter = vbNewLine
+        OpenFileDialog1.Filter = "All Files (*.*)|*.*"
+        OpenFileDialog1.ShowDialog()
+        CSVFile = OpenFileDialog1.FileName
     End Sub
 End Class
